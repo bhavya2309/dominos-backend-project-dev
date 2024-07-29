ba448a163f08d0e2e25a6285478a87372833d805
bhavya2309 <jainbhavya2309@gmail.com>
2024-07-29 20:13:39 +0530

    products api developed

---
+++ modules/products/models/product-schema.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+const sizeSchema = new mongoose.Schema({
+    size: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    }
+  });
+  
+const ProductSchema = mongoose.Schema({
+    // userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    // items: [ItemSchema], // Array of items in the order
+    // payment: PaymentSchema, // Payment details
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true
+      },
+      image: {
+        type: String,
+        required: true
+      },
+      ingredients: {
+        type: [String],
+        required: true
+      },
+      sizes: {
+        type: [sizeSchema],
+        required: true
+      }
+    });
+    
+  export const ProductModel = mongoose.model('products', ProductSchema);
